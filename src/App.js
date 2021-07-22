dc9f984acbbd1f15523b02aa209dbd5e4d9ad00e
Michael Halle <m@halle.us>
2021-07-22 14:01:04 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,197 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import DeckGL from '@deck.gl/react';
+
+import { ScatterplotLayer, GeoJsonLayer } from '@deck.gl/layers';
+import { HeatmapLayer } from '@deck.gl/aggregation-layers';
+import { StaticMap } from 'react-map-gl';
+import { Layout, Checkbox } from 'antd';
+import useSWR from "swr";
+import 'antd/dist/antd.css';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+import './App.css'
+
+const DataURLs = [
+  'https://raw.githubusercontent.com/NewtonMAGIS/GISData/master/Addresses/Addresses.geojson',
+  'https://raw.githubusercontent.com/NewtonMAGIS/GISData/master/Villages/Villages.geojson',
+  'https://raw.githubusercontent.com/NewtonMAGIS/GISData/master/Open%20Space/OpenSpace.geojson',
+  'https://raw.githubusercontent.com/NewtonMAGIS/GISData/master/Surface%20Waters/SurfaceWater.geojson'
+];
+
+const DisplayOptions = ['Parks', 'Water', 'Village centers'];
+
+const { Header, Content, Sider } = Layout;
+
+// Set your mapbox access token here
+const MAPBOX_ACCESS_TOKEN =
+  'pk.eyJ1IjoiaGFsYXphciIsImEiOiJja2N0dXI2Y3kxbTBoMnBxcTJnaTl3czVxIn0.MXzwZHuwNaOPKZgO17_YmA';
+
+// Viewport settings
+const INITIAL_VIEW_STATE = {
+  longitude: -71.207283,
+  latitude: 42.330,
+  zoom: 12,
+  pitch: 0,
+  bearing: 0
+};
 
 function App() {
+
+  const [selectedAddressTypes, setSelectedAddressTypes] = useState(['Apartment', 'Mixed', 'Residential']);
+  const [filteredAddresses, setFilteredAddresses] = useState([]);
+  const [selectedToDisplay, setSelectedToDisplay] = useState(DisplayOptions);
+
+  const { data } = useSWR([DataURLs, 'data'], fetchAll, { revalidateOnFocus: false });
+
+  const addressTypes = data ? data[0].map(x => x.properties.AddressType)
+    .filter(onlyUnique)
+    .filter(x => (x !== 'Misspelled'))
+    .sort() : [];
+
+  useEffect(() => {
+    if(data){
+    setFilteredAddresses(data[0].filter(x => 
+        selectedAddressTypes.includes(x.properties.AddressType)));
+    }
+  }, [selectedAddressTypes, data]);
+
+
+  const villageCenters = new ScatterplotLayer({
+    id: 'villageCenters',
+    visible: selectedToDisplay.includes('Village centers'),
+    data: data ? data[1] : null,
+    opacity: 1,
+    radiusUnits: 'pixels',
+    lineWidthUnits: 'pixels',
+    radiusMinPixels: 8,
+    radiusMaxPixels: 16,
+    stroked: true,
+    filled: true,
+    pickable: true,
+    getRadius: d => 5,
+    getFillColor: d => [180, 180, 200],
+    getLineColor: d => [255, 255, 255],
+
+    getPosition: d => d.geometry.coordinates,
+  });
+
+  const addressLayer = new HeatmapLayer({
+    id: 'address-layer',
+    opacity: 0.65,
+    data: filteredAddresses,
+    getPosition: d => d.geometry.coordinates,
+    getWeight: 1,
+    aggregation: 'SUM'
+  });
+
+  const parksLayer = new GeoJsonLayer({
+    id: 'parks-layer',
+    visible: selectedToDisplay.includes('Parks'),
+    data: data ? data[2] : [],
+    pickable: false,
+    stroked: false,
+    filled: true,
+    extruded: false,
+    lineWidthScale: 20,
+    lineWidthMinPixels: 2,
+    getFillColor: [50, 255, 50, 60],
+    getLineColor: [0, 0, 0],
+    getRadius: 10,
+    getLineWidth: 1,
+    getElevation: 30
+  });
+
+  const waterLayer = new GeoJsonLayer({
+    id: 'water-layer',
+    visible: selectedToDisplay.includes('Water'),
+    data: data ? data[3] : [],
+    pickable: false,
+    stroked: false,
+    filled: true,
+    extruded: false,
+    lineWidthScale: 20,
+    lineWidthMinPixels: 2,
+    getFillColor: [10, 10, 230, 60],
+    getLineColor: [0, 0, 0],
+    getRadius: 10,
+    getLineWidth: 1,
+    getElevation: 30
+  });
+
+  const layers = [addressLayer, villageCenters, parksLayer, waterLayer];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout style={{}}>
+        <Header style={{  height: "64px", paddingLeft: '10px', backgroundColor: "white" }} >
+          <h1>Newton address heat map</h1>
+        </Header>
+        <Layout>
+      <Sider
+        width={230}
+        style={{
+          overflow: 'auto',
+          height: '100vh',
+          position: 'fixed',
+          backgroundColor: 'white',
+          left: 0,
+        }}
+      >
+        <div className="side-content">
+        <h2>Display</h2>
+          <Checkbox.Group
+            className="display"
+            options={DisplayOptions}
+            defaultValue={selectedToDisplay}
+            onChange={setSelectedToDisplay}
+          />
+          <h2>Address Types</h2>
+          <Checkbox.Group
+            className="address-types"
+            options={addressTypes}
+            defaultValue={selectedAddressTypes}
+            onChange={setSelectedAddressTypes}
+          />
+          <div class="credit">
+            Data courtesy <a href="https://github.com/NewtonMAGIS/GISData">Newton GIS via Github</a>.
+          </div>
+        </div>
+
+      </Sider >
+      <Layout className="site-layout" style={{ height: '100%', marginLeft: 230 }}>
+
+        <Content style={{ height: "91vh", position: "relative" }}>
+          <DeckGL
+            initialViewState={INITIAL_VIEW_STATE}
+            controller={true}
+            layers={layers}
+          >
+            <StaticMap
+              mapStyle='mapbox://styles/mapbox/dark-v10'
+              mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN} />
+          </DeckGL>
+        </Content>
+      </Layout>
+    </Layout >
+    </Layout>
+
+
   );
 }
 
-export default App;
+function onlyUnique(value, index, self) {
+  return self.indexOf(value) === index;
+}
+
+function fetcher(url) {
+  return fetch(url)
+    .then(r => r.json())
+    .then(j => {
+      return j.features;
+    });
+}
+
+function fetchAll(urls) {
+  return Promise.all(urls.map(fetcher));
+}
+
+export default App
